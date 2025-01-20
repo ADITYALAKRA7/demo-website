b1861cb0950b9b39385b7bfb6739dddc5ec97bce
ADITYALAKRA7 <108651080+ADITYALAKRA7@users.noreply.github.com>
2025-01-20 15:53:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Wait until the DOM is fully loaded
+document.addEventListener("DOMContentLoaded", () => {
+    // Get the button element
+    const popupButton = document.getElementById("popup-button"); 
+    // Add a click event listener to the button
+    popupButton.addEventListener("click", () => {
+    // Display a popup message
+    alert("Hello! aditya how are you This is your popup message.");
+    });
+    });
+    
